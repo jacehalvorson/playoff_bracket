495bff7491000d6ec4bbb5bdd05994ed63b88223
jacehalvorson <82925305+jacehalvorson@users.noreply.github.com>
2024-12-23 22:06:20 -0600

    Import Playoff Bracket app from jacehalvorson/portfolio
    https://github.com/jacehalvorson/portfolio
    
    Create API for DynamoDB access

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,45 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apiplayoffbrackets": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "getPlayoffBrackets": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "playoffteams": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "playoffbrackets": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    },
+    "playoffteams": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
